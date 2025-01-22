c6d83a4adbe02038ae44dda3a9e57d12e40d7b71
carles <carles.gallardo00@gmail.com>
2025-01-22 16:03:26 +0100

    picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css'
+import "./App.css";
+import profilePicture from "./assets/profile.jpeg";
 
 function App() {
   return (
     <>
-      <h1>Carles Gallardo</h1>
-      <div>
-        
+      <div className="first-page">
+        <article className="greetings">
+          <p>
+            Hi! my name is <b className="highlight">Carles</b> and I am a
+            Frontend Developer specialized in React.
+          </p>
+        </article>
+        <article className="profile">
+          <img className="profilePicture" src={profilePicture} alt="" />
+        </article>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
