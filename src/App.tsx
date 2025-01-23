a04d4cd5da9360e8631f0fe8c5ba28f03eb9187f
Carles Gallardo <carles.gallardo00@gmail.com>
2025-01-23 09:06:28 +0100

    animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import "./App.css";
 import profilePicture from "./assets/profile.jpeg";
+import Github from "./svgs/Github";
+import Gmail from "./svgs/Gmail";
+import LinkedIn from "./svgs/Linkedin";
 
 function App() {
   return (
     <>
       <div className="first-page">
         <article className="greetings">
-          <p>
-            Hi! my name is <b className="highlight">Carles</b> and I am a
+          <p className="appear-left">
+            Hi! My name is <b className="highlight">Carles</b> and I am a
             Frontend Developer specialized in React.
           </p>
         </article>
         <article className="profile">
-          <img className="profilePicture" src={profilePicture} alt="" />
+          <img className="profilePicture" src={profilePicture} alt="Carles Gallardo Ramirez"/>
+          <div className="links">
+            <a className="linkedin" href="https://www.linkedin.com/in/carles-gallardo-ramirez/" target="_blank">
+              <LinkedIn/>
+            </a>
+            <a className="github" href="https://github.com/carles00" target="_blank">
+              <Github/>
+            </a>
+            <a className="gmail" href="mailto:carles.gallardo00@gmail.com">
+              <Gmail/>
+            </a>
+          </div>
         </article>
       </div>
     </>
