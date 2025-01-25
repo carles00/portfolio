0cc6d426c1cbd52e5a88952830c1bba759724928
Carles Gallardo <carles.gallardo00@gmail.com>
2025-01-25 13:39:08 +0100

    style change and info

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,63 @@ import LinkedIn from "./svgs/Linkedin";
 function App() {
   return (
     <>
-      <div className="first-page">
-        <article className="greetings">
-          <p className="appear-left">
-            Hi! My name is <b className="highlight">Carles</b> and I am a
-            Frontend Developer specialized in React.
-          </p>
-        </article>
-        <article className="profile">
-          <img className="profilePicture" src={profilePicture} alt="Carles Gallardo Ramirez"/>
-          <div className="links">
-            <a className="linkedin" href="https://www.linkedin.com/in/carles-gallardo-ramirez/" target="_blank">
-              <LinkedIn/>
-            </a>
-            <a className="github" href="https://github.com/carles00" target="_blank">
-              <Github/>
-            </a>
-            <a className="gmail" href="mailto:carles.gallardo00@gmail.com">
-              <Gmail/>
-            </a>
+      <article className="greetings">
+        <p className="appear-left presentation">
+          Hi! My name is <b className="highlight">Carles</b> and I am a Frontend
+          Developer
+        </p>
+        <div></div>
+      </article>
+      <article className="profile">
+        <img
+          className="profilePicture"
+          src={profilePicture}
+          alt="Carles Gallardo Ramirez"
+        />
+        <div className="links">
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/carles-gallardo-ramirez/"
+            target="_blank"
+          >
+            <LinkedIn />
+          </a>
+          <a
+            className="github"
+            href="https://github.com/carles00"
+            target="_blank"
+          >
+            <Github />
+          </a>
+          <a className="gmail" href="mailto:carles.gallardo00@gmail.com">
+            <Gmail />
+          </a>
+        </div>
+      </article>
+      <article className="experience">
+          <div className="card">
+            <h2 className="card-title">Experience</h2>
+            <hr className="card-divider"/>
+            <div className="card-body">
+              <p className="card-body-title">Frontend Developer at Itequia, 2023 - Present</p>
+              <p>
+              Started the development of Akute, a CRM Saas. Developed as a SPA using React, I was responsible for the architecture of the frontend, setting up the test environment using Jest and creating CI/CD pipelines.
+              At TeamInsights I was responsible for the migration of the website to a modern standard using Angular, making sure to follow best practices and writing clean and maintainable code. During this time I improved the load time of the website as well as improved the developer experience by creating highly reusable components.
+              </p>
+            </div>
           </div>
-        </article>
-      </div>
+          <div className="card">
+            <h2 className="card-title">Education</h2>
+            <hr className="card-divider"/>
+            <div className="card-body">
+              <p className="card-body-title">Universitat Pompeu Fabra, 2018 - 2023</p>
+              <p>
+                Bachelor's degree in Computer Engineering
+              </p>
+            </div>
+          </div>
+      </article>
+      <article className="professional"></article>
     </>
   );
 }
